8d78f7341addda16e077464b8ce758e4e60c0f38
Vladislav <vladokatr@gmail.com>
2019-06-05 00:44:36 +0300

    added logic for CSV generation. Should refactor it to different methods

---
+++ src/main/java/com/personal/Main.java
@@ -0,0 +1,108 @@
+package com.personal;
+
+import com.google.gson.Gson;
+import com.opencsv.CSVWriter;
+import com.personal.model.Report;
+import com.personal.model.Salesman;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String pathToSalesmanList;
+        String pathToReportList;
+        if (args.length == 2) {
+            pathToSalesmanList = args[0];
+            pathToReportList = args[1];
+            String salesmenJsonStr = "  [\n" +
+                    "{\n" +
+                    "\"name\": \"John Smith\",\n" +
+                    "\"totalSales\": 250,\n" +
+                    "\"salesPeriod\": 10,\n" +
+                    "\"experienceMultiplier\": 0.5\n" +
+                    "},\n" +
+                    "{\n" +
+                    "\"name\": \"David Prowless\",\n" +
+                    "\"totalSales\": 230,\n" +
+                    "\"salesPeriod\": 10,\n" +
+                    "\"experienceMultiplier\": 0.5\n" +
+                    "}\n" +
+                    "]";/*sendGet(pathToSalesmanList);*/
+            String reportJsonStr = "{\n" +
+                    "\"topPerformersThreshold\": 600,\n" +
+                    "\"useExperienceMultiplier\": true,\n" +
+                    "\"periodLimit\": 10\n" +
+                    "}";/*sendGet(pathToReportList);*/
+            Gson gson = new Gson();
+
+            Salesman[] salesArr = gson.fromJson(salesmenJsonStr, Salesman[].class);
+            Report report = gson.fromJson(reportJsonStr, Report.class);
+
+            generateCSV("Result.csv", new ArrayList<Salesman>(Arrays.asList(salesArr)), report);
+        } else {
+            System.out.println("Please, provide paths for both the Salesman list and for ReportList");
+            return;
+        }
+
+
+    }
+
+    private static void generateCSV(String fileName, ArrayList<Salesman> salesmen, Report report) throws IOException {
+        File file = new File(fileName);
+        FileWriter outputfile = new FileWriter(file);
+
+        // create CSVWriter object filewriter object as parameter
+        CSVWriter writer = new CSVWriter(outputfile);
+
+        // adding header to csv
+        String[] header = {"Name", "Score"};
+        writer.writeNext(header);
+
+        salesmen.sort(Salesman::customCompare);
+        double x = (report.getTopPerformersThreshold().doubleValue() / 100);
+
+        //if the threshold is more than 100 percent, then index out of bounds
+        int xPos = x <= 1 ?(int) (salesmen.size() * x) : salesmen.size();
+        for (int i = 0; i < xPos; i++) {
+            if (salesmen.get(i).getSalesPeriod() <= report.getPeriodLimit()) {
+                Salesman s = salesmen.get(i);
+                double score = report.isUseExperienceMultiplier() ?
+                        s.getTotalSales().doubleValue()
+                                / s.getSalesPeriod().doubleValue() * s.getExperienceMultiplier() :
+                        s.getTotalSales().doubleValue() / s.getSalesPeriod().doubleValue();
+                String[] data = {s.getName(), String.valueOf(score)};
+                writer.writeNext(data);
+            }
+        }
+
+        // closing writer connection
+        writer.close();
+
+    }
+
+    private static String sendGet(String url) throws IOException {
+        URL urlSales = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) urlSales.openConnection();
+        connection.setRequestMethod("GET");
+        int responseCode = connection.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            return response.toString();
+        } else {
+            System.out.println("Something went wrong");
+            return null;
+        }
+    }
+}
--- src/main/java/com/personal/model/Salesman.java
+++ src/main/java/com/personal/model/Salesman.java
@@ -3,6 +3,7 @@ package com.personal.model;
 public class Salesman {
     private String name;
     private Integer totalSales;
+    private Integer salesPeriod;
     private Double experienceMultiplier;
 
     public String getName() {
@@ -21,11 +22,23 @@ public class Salesman {
         this.totalSales = totalSales;
     }
 
+    public Integer getSalesPeriod() {
+        return salesPeriod;
+    }
+
+    public void setSalesPeriod(Integer salesPeriod) {
+        this.salesPeriod = salesPeriod;
+    }
+
     public Double getExperienceMultiplier() {
         return experienceMultiplier;
     }
 
     public void setExperienceMultiplier(Double experienceMultiplier) {
         this.experienceMultiplier = experienceMultiplier;
+    }
+
+    public static int customCompare(Salesman s2, Salesman s1){
+        return (int) ((s1.getTotalSales().doubleValue()/s1.getSalesPeriod().doubleValue()) - (s2.getTotalSales().doubleValue()/s2.getSalesPeriod().doubleValue()));
     }
 }
